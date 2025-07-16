2e0565081006f117156ef9b50e86bc98ebcb5a6f
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-07-16 16:37:40 +0300

    not used

--- src/routes/customer/order-details.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/customer/order-details')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/customer/order-details"!</div>
-}
