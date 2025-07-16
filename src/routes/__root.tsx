53ddd2d3d0e0f5419fae04079b185b9b9042013c
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-07-16 16:28:20 +0300

    Add Toaster component for enhanced user notifications

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx'
 import type { QueryClient } from '@tanstack/react-query'
 import { ThemeProvider } from '@/providers/theme-provider'
+import { Toaster } from 'sonner'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -12,6 +13,14 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <ThemeProvider defaultTheme="light" storageKey="freshcart-ui-theme">
+        <Toaster 
+          position="top-right"
+          richColors
+          closeButton
+          toastOptions={{
+            duration: 3000,
+          }}
+        />
         <Outlet />
         <TanStackRouterDevtools />
         <TanStackQueryLayout />
