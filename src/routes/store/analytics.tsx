5b4078ac41390bcdf6fa4a1cea7ee5f4da986a85
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-07-21 12:16:12 +0300

    Store sales analytics

--- src/routes/store/analytics.tsx
+++ src/routes/store/analytics.tsx
@@ -1,9 +1,26 @@
+import OrdersLineChart from '@/components/ordersChart';
+import { useStoreOrders } from '@/hooks/useOrders';
 import { createFileRoute } from '@tanstack/react-router'
+
 
 export const Route = createFileRoute('/store/analytics')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/store/analytics"!</div>
+  const store = localStorage.getItem("currentStore") || '';
+  const storeId = store ? JSON.parse(store).store_id : 0;
+  const ordersData = useStoreOrders(storeId);
+  return (
+    <>
+      {/* Orders Chart */}
+      <div className="p-6">
+        <OrdersLineChart orders={ordersData.data} />
+      </div>
+      {/* Revenue Chart */}
+      {/* <div className="p-6">
+        <RevenueChart revenue={revenueData} />
+      </div> */}
+    </>
+  );
 }
